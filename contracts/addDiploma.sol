b738adcc8aa600a4fd2b35cdd5b43ffc15a03359
Cyelliott <cyelliott0@gmail.com>
2019-04-06 14:24:11 -0400

    Update addDiploma.sol

--- contracts/addDiploma.sol
+++ contracts/addDiploma.sol
@@ -1,23 +1,26 @@
-pragma solidity >=0.4 .22 <0.6.0;
+pragma solidity >-0.4 .22 <0.6.0;
+contract Diploma { 
 address owner; 
 mapping {address=>Diploma} info; 
 struct Diploma { 
     
-     name; 
-    address; 
-     degree;
-     date; 
+     string name; 
+    address studentAddress; 
+     string degree;
+     uint date; 
     
 } 
 constructor () public { 
     owner = msg.sender 
 } 
-function addDiploma(address address, string name, string degree, uint date); { 
+function addDipoloma(address _address, string name, string degree, uint date); { 
 assert(msg.sender == owner); 
-info[address].address = address; 
+info[address].studentAddress = _address; 
 info[address].degree = degree; 
 info[address].date = date; 
 info[address].name = name; 
 
 } 
+} 
+    
     
