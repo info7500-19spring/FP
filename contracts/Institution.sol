2f47b18ee1ca827018327d5261d29d3baf88157e
Hoyin Chu <chu.ho@blocktest.net>
2019-04-10 18:10:24 -0400

    renaming and refactoring

---
+++ contracts/Institution.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.5.0;
+
+contract Institution {
+
+    // Fields of a diploma
+    struct Diploma {  
+        string name; 
+        address studentAddress; 
+        string degree;
+        uint date; 
+    } 
+
+    address owner;
+
+    // Person => Diploma of Person
+    mapping(address => Diploma) info;
+
+    constructor () public { 
+        owner = msg.sender;
+    } 
+
+    // Add a new Diploma
+    function addDipoloma(address _address, string memory _name, string memory _degree, uint _date) public { 
+        require(msg.sender == owner); 
+        info[_address].studentAddress = _address; 
+        info[_address].degree = _degree; 
+        info[_address].date = _date; 
+        info[_address].name = _name; 
+    }
+
+    // View diploma of given person
+    function viewDiploma(address _address) public view returns(string memory, address, string memory, uint) {
+        Diploma storage diploma = info[_address];
+        return (diploma.name, diploma.studentAddress, diploma.degree, diploma.date);
+    }
+}
+
+    
+    
