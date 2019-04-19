0158b87c4d2633d5e3b45d7ae6b7a933a94fbae3
lingfengzhou <zhou.ling@husky.neu.edu>
2019-04-19 16:35:49 -0400

    add dp

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import platformContract from "./contracts/platformContract.json";
+// import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import platformContract from "./contracts/Platform.json";
 import getWeb3 from "./utils/getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      storageValue: 0,
+      web3: null,
+      accounts: null,
+      contract: null,
+      studentAddress:'',
+      degree:'',
+      date:''
+    };
+    this.addToSimpleStorage = this.addToSimpleStorage.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
   componentDidMount = async () => {
     try {
@@ -18,7 +32,8 @@ class App extends Component {
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      // const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const deployedNetwork = platformContract.networks[networkId];
       // const instance = new web3.eth.Contract(
       //   SimpleStorageContract.abi,
       //   deployedNetwork && deployedNetwork.address,
@@ -35,7 +50,7 @@ class App extends Component {
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
+        `Failed to load web3, accounts, or contract. Check console for details.`
       );
       console.error(error);
     }
@@ -54,6 +69,61 @@ class App extends Component {
   //   this.setState({ storageValue: response });
   // };
 
+  addToSimpleStorage() {
+    if (this.state.contract && this.state.accounts) {
+      const value = this.storageAmountInput.value;
+      console.log('value to be stored is');
+      console.log(value);
+      this.state.contract.methods.set(value).send({from: this.state.accounts[0]})
+        .then((result) => {
+          return this.state.contract.methods.get().call()
+        }).then((result) => {
+          this.setState({
+            storageValue: result
+          });
+        }).catch((err) => {
+          console.log('error');
+          console.log(err);
+        });
+    } else {
+      this.setState(prevState => ({
+        ...prevState,
+        error: new Error('simple storage instance not loaded')
+      }))
+    }
+  }
+  addToplatform() {
+    if (this.state.contract && this.state.accounts) {
+      const studentAddress = this.state.studentAddress;
+      const degree = this.state.degree;
+      const date = this.state.date;
+      console.log(studentAddress);
+      console.log(degree);
+      console.log(date);
+      this.state.contract.methods.addDipoloma(studentAddress,degree,date).send({from: this.state.accounts[0]})
+        .then((result) => {
+          console.log(result)
+        }).catch((err) => {
+          console.log('error');
+          console.log(err);
+        });
+    } else {
+      this.setState(prevState => ({
+        ...prevState,
+        error: new Error('instance not loaded')
+      }))
+    }
+  }
+
+  handleChange(event){
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
@@ -71,6 +141,44 @@ class App extends Component {
           Try changing the value stored on <strong>line 40</strong> of App.js.
         </p>
         <div>The stored value is: {this.state.storageValue}</div>
+        <h2>Interactive Dapp Example</h2>
+        <p>
+          You should be able to use this form to interact with the storage smart
+          contract.
+        </p>
+        <form className="pure-form pure-form-stacked" onSubmit= {(e) => {
+                e.preventDefault();
+                // this.addToSimpleStorage();
+                this.addToplatform();
+              }}>
+            <label> student address:
+            <input
+              name="studentAddress"
+              type="text"
+              value={this.state.studentAddress}
+              onChange={this.handleChange}
+            />
+            </label>
+            <br />
+            <label> degree:
+            <input
+              name="degree"
+              type="text"
+              value = {this.state.degree}
+              onChange={this.handleChange}
+            />
+            </label>
+            <br />
+            <label> date:
+            <input
+              name="date"
+              type="text"
+              value={this.state.date}
+              onChange={this.handleChange}
+            />
+            </label>
+            <input type = "submit" value = "Submit"/>
+        </form>
       </div>
     );
   }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,12 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var DiplomaViewer = artifacts.require("./DiplomaViewer.sol");
+var Institution = artifacts.require("./Institution.sol");
+var Person = artifacts.require("./Person.sol");
+var PlatForm = artifacts.require("./Platform.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(PlatForm)
+  // .then(() => deployer.deploy(DiplomaViewer))
+  // .then(() => deployer.deploy(Institution))
+  // .then(() => deployer.deploy(Person))
 };
